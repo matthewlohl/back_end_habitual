9babf4e2ae8940035566a1a3257d1feabb9d7142
Matthew Lo <matthewlol316@gmail.com>
2022-09-21 10:42:06 +0100

    change run to query in sql

--- dbConfig/seedDev.js
+++ dbConfig/seedDev.js
@@ -3,4 +3,4 @@ const fs = require('fs');
 
 const seeds = fs.readFileSync(__dirname + '/dev_seeds.sql').toString();
 
-db.run(seeds, () => console.log('Dev database seeded'));
+db.query(seeds, () => console.log('Dev database seeded'));
