2cddfd40cb644d836f9e53da7976236e943404d0
Leehamb99 <leehamb99@gmail.com>
2022-09-22 13:00:03 +0100

    add daily reset function

--- dbConfig/dev_seeds.sql
+++ dbConfig/dev_seeds.sql
@@ -1,13 +1,15 @@
-INSERT INTO habits(habit_name, date_complete, period, frequency, frequency_done, user_id)
+INSERT INTO habits(habit_name, date_complete, period, frequency, frequency_done, streak, user_id)
 VALUES
+    ('drink water', '1998-04-16', 1, 5, 2, NULL , 4),
+    ('gym', '1998-04-16', 1, 3, 1 ,NULL , 3),
+    ('basketball', '1998-04-16', 2, 2, 2, NULL , 3),
+    ('football', '2022-09-22', 1, 3, 3,NULL , 1),
+    ('swimming', '1998-04-16', 1, 3, 0,NULL , 1),
+    ('golf', '1998-04-16', 4, 1, 0,NULL , 2);
 
-    ('drink water', '{20220101, 20220102, 20220103}', 1, 17, 0),
-    ('gym', '{20220105, 20220107, 20220108}', 1, 30, 0),
-    ('basketball', '{20220211, 20220312, 20220320}', 2, 22, 0);
 
 
-
-INSERT INTO users(user_name, email, password_digest)
+INSERT INTO users(user_name, email,password_digest)
 VALUES
     ('Matthew', 'matthew@example.com', '123123'),
     ('Bradley', 'bradley@example.com', 'abcabc'),
