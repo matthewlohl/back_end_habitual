5a8ebc1ab920ac237036d12a4d192bdd981a8abe
Camila Pessoa <caape13@hotmail.com>
2022-09-22 12:06:35 +0100

    add config, test_sseds and first habit route test

---
+++ tests/integration/config.js
@@ -0,0 +1,22 @@
+const request = require('supertest');
+const fs = require('fs')
+const app = require('../../server');
+const { Pool } = require('pg');
+
+const testSeed = fs.readFileSync(__dirname + '/test_seeds.sql').toString();
+
+const resetTestDB = () => {
+  return new Promise (async (resolve, reject) => {
+    try {
+      const db = new Pool()
+      await db.query(testSeed);
+      resolve('Test DB reset');
+    } catch (err) {
+      reject(`Test DB could not be reset: ${err} in ${err.file}`);
+    }
+  });
+}
+
+global.request = request;
+global.app = app;
+global.resetTestDB = resetTestDB;
